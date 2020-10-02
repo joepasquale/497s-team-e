91aa915d3beacced5437c700cc9dd84e4f945b55
ron_arbo <ron.arbo63@icloud.com>
2020-10-01 20:57:25 -0400

    nginxPractice Added

---
+++ nginxPractice/flask/app/views.py
@@ -0,0 +1,36 @@
+# Import modules
+from app import app
+import requests
+from flask import Flask, request, jsonify
+
+# Define the API Key, Define the Endpoint, Define the Header
+API_KEY = '9DtY40amt8NwFuCTtVPweR9w10EJ2XYXi47mTBC4fXeIR2wBvJkLFKi5n20PO-OdgcGXoolgBqITpxyd8qh194tI7xM5mxjJYg3gURkgvAGzLq4GN_aIjcSGdLdzX3Yx'
+ENDPOINT = 'https://api.yelp.com/v3/businesses/search'
+HEADERS = {'Authorization': 'bearer %s' % API_KEY}
+
+@app.route("/")
+def index():
+    return "Hello from Flask"
+
+@app.route("/yelp")
+def yelp_resp():
+
+    terms = request.args.get('terms')
+    location = request.args.get('location')
+
+    PARAMETERS = {'terms': terms,
+    'limit': 10,
+    'location': location}
+
+    # Make a request to the yelp api
+    response = requests.get(url = ENDPOINT, params= PARAMETERS, headers= HEADERS)
+
+    # Convert JSON string to dictionary
+    business_data = response.json()
+    businessArr = []
+
+    for biz in business_data['businesses']:
+        businessArr.append(biz['name'])
+
+    businessStr = ' '.join(map(str, businessArr))
+    return(businessStr)
