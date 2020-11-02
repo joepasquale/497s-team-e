fee9efd4e99983713828493d698fb521342b7828
tjgoldblatt <tj@psyton.com>
2020-11-02 18:17:17 -0500

    Added user input for Yelp API and Creating in Groups DB

--- linkup-ui/src/Pages/Homepage.jsx
+++ linkup-ui/src/Pages/Homepage.jsx
@@ -1,64 +1,159 @@
 import React from 'react';
 import {Button} from 'react-bootstrap';
+import {Form, Col, Card} from 'react-bootstrap';
 import axios from 'axios';
+
+import MenuBar from '../Components/MenuBar';
 export class HomePage extends React.Component {
   constructor(props) {
     super(props);
+    this.mocking = true;
+    this.data = this.mocking
+      ? {
+          terms: '',
+          location: '',
+          groupID: '',
+          groupName: '',
+          groupMembers: '',
+        }
+      : {};
     this.state = {
       response: '',
+      groupResponse: '',
     };
   }
+  handleSubmitGroup = () => {
+    const id = document.getElementById('groupID').value;
+    const name = document.getElementById('groupName').value;
+    const members = document.getElementById('groupMembers').value;
 
-  //Will use something like this once we actually start implementing webpages with 'submit' buttons to press
-  // handleSubmit = () => {
-  //   const toSend = {
-  //     terms: "restaurant",
-  //     location: "Boston"
-  //   };
-  //   const instance = axios.create({timeout: 10000});
-  //   instance.defaults.headers.common['Content-Type'] = 'application/json';
-  //   instance.post('/yelp', toSend).
-  //   then((res) => {
-  //     this.setState({response: res});
-  //   }).catch((err) => {
-  //     this.setState({response: err});
-  //   });
-  // }
-  componentDidMount() {
+    const groupMembers = members.split(',').map(member => member.trim());
+
     const toSend = {
-      terms: 'restaurant',
-      location: 'Boston',
+      groupID: id,
+      groupName: name,
+      groupMembers: groupMembers,
     };
+    const outer = this;
     const instance = axios.create({timeout: 10000});
+    console.log(toSend);
+    instance.defaults.headers.common['Content-Type'] = 'application/json';
+    instance
+      .post('http://' + window.location.hostname + '/group/create', toSend)
+      .then(res => {
+        console.log(res);
+        outer.setState({groupResponse: res});
+      })
+      .catch(err => {
+        console.log(err);
+        outer.setState({groupResponse: err});
+      });
+  };
+  handleSubmit = () => {
+    const terms = document.getElementById('termsId').value;
+    const location = document.getElementById('locationId').value;
+    const toSend = {
+      terms: terms,
+      location: location,
+    };
+    const outer = this;
+    const instance = axios.create({timeout: 10000});
+    console.log(toSend);
     instance.defaults.headers.common['Content-Type'] = 'application/json';
     instance
       .post('http://' + window.location.hostname + '/yelp', toSend)
       .then(res => {
-        this.setState({response: res});
+        console.log(res);
+        outer.setState({response: res});
       })
       .catch(err => {
-        this.setState({response: err});
+        console.log(err);
+        outer.setState({response: err});
       });
-  }
+  };
+
   render() {
-    const {response} = this.state;
-    console.log(response.data);
-    let showButton = '';
-    if (response.data !== undefined) {
-      showButton = (
-        <div>
-          <br />
-          <Button>Post Successful</Button>
-        </div>
-      );
-    }
+    const {response, groupResponse} = this.state;
+    console.log(groupResponse);
     return (
       <div>
-        <div>
-          <h1>Making Yelp Post Request</h1>
-          {response.data}
-        </div>
-        <div>{showButton}</div>
+        <MenuBar />
+        <br />
+        <Card>
+          <Card.Body>
+            <Card.Title>Access Yelp API</Card.Title>
+            <Form>
+              <Form.Group>
+                <Col xs="5">
+                  <Form.Label>Enter Activity</Form.Label>
+                  <Form.Control type="text" id="termsId" />
+                </Col>
+              </Form.Group>
+
+              <Form.Group>
+                <Col xs="5">
+                  <Form.Label>Enter Location</Form.Label>
+                  <Form.Control type="text" id="locationId" />
+                </Col>
+              </Form.Group>
+              <Col>
+                <Button
+                  variant="success"
+                  type="button"
+                  onClick={e => this.handleSubmit(e)}
+                >
+                  Submit
+                </Button>
+                <div>
+                  <br />
+                  {response.data}
+                </div>
+              </Col>
+            </Form>
+          </Card.Body>
+        </Card>
+
+        <Card>
+          <Card.Body>
+            <Card.Title>Create A Group</Card.Title>
+            <Form>
+              <Form.Group>
+                <Col xs="5">
+                  <Form.Label>Group ID</Form.Label>
+                  <Form.Control type="text" id="groupID" />
+                </Col>
+              </Form.Group>
+
+              <Form.Group>
+                <Col xs="5">
+                  <Form.Label>Group Name</Form.Label>
+                  <Form.Control type="text" id="groupName" />
+                </Col>
+              </Form.Group>
+
+              <Form.Group>
+                <Col xs="5">
+                  <Form.Label>Group Members</Form.Label>
+                  <Form.Control type="text" id="groupMembers" />
+                </Col>
+              </Form.Group>
+
+              <Col>
+                <Button
+                  variant="success"
+                  type="button"
+                  onClick={e => this.handleSubmitGroup(e)}
+                >
+                  Submit Group
+                </Button>
+                <div>
+                  <br />
+                  {groupResponse.data}
+                </div>
+              </Col>
+            </Form>
+          </Card.Body>
+        </Card>
       </div>
     );
   }
