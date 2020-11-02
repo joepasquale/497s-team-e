652c2a8614598ce675cd4b7fcce2111c3f9b7f65
joepasquale <jpasquale@umass.edu>
2020-11-02 12:21:45 -0500

    saved gcal api key and client id

--- googleCal/app/views.py
+++ googleCal/app/views.py
@@ -5,8 +5,9 @@ import requests
 from flask import Flask, request, jsonify
 
 # Define the API Key, Define the Endpoint, Define the Header
-API_KEY = 
-ENDPOINT = 
+API_KEY = 'AIzaSyDdVUMKArJ1GpFar9QbxuZWvFLzROVqI0g'
+CLIENT_ID = '350429252210-abjunlthml9hfblfhv9rb949s1vra1u0.apps.googleusercontent.com'
+SCOPE = "https://www.googleapis.com/auth/calendar.readonly""
 HEADERS = {'Authorization': 'bearer %s' % API_KEY}
 
 CORS(app)
