1d58e0b6c22bc2b836c7ca9ce20ffd1e9fa36fb3
tjgoldblatt <tj@psyton.com>
2020-11-04 18:08:17 -0500

    Added frontend CRUD operations for events db

--- linkup-ui/src/Components/MenuBar.jsx
+++ linkup-ui/src/Components/MenuBar.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import {Navbar, Nav, NavDropdown} from 'react-bootstrap';
+// import Link from 'react-router-dom/Link';
 
 export class MenuBar extends React.Component {
   render() {
@@ -9,23 +10,7 @@ export class MenuBar extends React.Component {
           <Navbar.Brand href="#">LinkUp</Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="mr-auto">
-              <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#link">Link</Nav.Link>
-              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">
-                  Another action
-                </NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">
-                  Something
-                </NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4">
-                  Separated link
-                </NavDropdown.Item>
-              </NavDropdown>
-            </Nav>
+            <Nav className="mr-auto"></Nav>
           </Navbar.Collapse>
         </Navbar>
       </div>
